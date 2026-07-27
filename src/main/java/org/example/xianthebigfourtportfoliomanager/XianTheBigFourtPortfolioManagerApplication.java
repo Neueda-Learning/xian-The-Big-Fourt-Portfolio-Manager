0d9e94b4cdaeac9f1191f130b244cc2bd939677e
Jason <yue450347@163.com>
2026-07-27 03:10:01 +0000

    initial architecture

---
+++ src/main/java/org/example/xianthebigfourtportfoliomanager/XianTheBigFourtPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.xianthebigfourtportfoliomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianTheBigFourtPortfolioManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XianTheBigFourtPortfolioManagerApplication.class, args);
+    }
+
+}
